603510df1dcd7229220e0f76c5f94eee097f17c8
Your Name <you@example.com>
2018-10-01 14:33:07 +0900

    fix ssh key name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 server '54.178.246.51', user: 'ec2-user', roles: %w{app db web}
 set :ssh_options, {
-   keys: [File.expand_path('~/.ssh/pair_key.pem)')]
+   keys: [File.expand_path('~/.ssh/key_pair.pem)')]
 }
