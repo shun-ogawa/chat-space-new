f7e989434f68db37e7e0e8b62e9e09848e00efd2
Your Name <you@example.com>
2018-10-16 12:10:10 +0900

    fix auto reloading system

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,51 +1,86 @@
 $(function() {
+
+  // 画面上最新のメッセージ
   var latestMessage = window.latestMessage;
   var alertMessage = $(".alert p");
 
+  // html要素追加処理
   function appendList(data) {
-    var image = (data.image != null) ? `<img style="margin-top: 10px" src="${data.image}"}` : ''
     var html = `<div class="message">
-                  <div class="message__user-name"
+                  <div class="message__user-name">
                     ${data.user_name}
                   </div>
-                  <div class="message__date" >
+                  <div class="message__date">
                     ${data.created_at}
-                  <div>
-                  <div class="message__text" >
-                      ${data.content}
-                      ${image}
                   </div>
-                </div>`
+                  <div class="message__text">
+                    ${data.content}
+                  </div>
+                  ${(data.image != null) ? `<img style="margin-top: 10px" src="${data.image}"}` : ''}
+                </div>`;
     $(".messages").append(html);
 
     var scrollTarget = $(".main").find($(".messages"));
-    console.log(scrollTarget);
     scrollTarget.animate({scrollTop: scrollTarget[0].scrollHeight}, 500, "swing");
   }
 
-  function reloadPage() {
-    var url = window.location.pathname;
-    console.log(url);
+  // メッセージ非同期通信投稿
+  $("#new_message").on("submit", function(e) {
+    e.preventDefault();
+    var input = $(".message").val();
+    var url = $(this).attr("action");
+    var formData = new FormData(this);
     $.ajax({
-      type: "GET",
+      type: 'POST',
       url: url,
-      data: {
-        latestMessage: latestMessage
-      },
-      dataType: "json"
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
 
-    .done(function(messages) {
-      messages.forEach(function(message) {
-        appendList(message);
-        latestMessage = message;
-      });
+    .done(function(data){
+      appendList(data);
+      $('.message').val('');
+      // 画面上の最新メッセージ更新
+      latestMessage = data;
+      // submitのdisbaled属性解除
+      $('.submit').attr('disabled', false);
     })
 
     .fail(function() {
-      alertMessage.text("接続に失敗しました！");
+      alert('メッセージの送信に失敗しました！');
     });
+  });
+
+  // メッセージ自動更新処理
+  function reloadPage() {
+    // chat画面でのみ処理実行
+    if(document.URL.match("messages")) {
+      var url = window.location.pathname;
+      $.ajax({
+        type: "GET",
+        url: url,
+        data: {
+          latestMessage: latestMessage
+        },
+        dataType: "json"
+      })
+
+      .done(function(messages) {
+        messages.forEach(function(message) {
+          appendList(message);
+          // 画面上の最新メッセージ更新
+          latestMessage = message;
+        });
+      })
+
+      .fail(function() {
+        alertMessage.text("接続に失敗しました！");
+      });
+    }
   }
 
+  // 5秒ごとにメッセージ自動更新呼び出し
   setInterval(reloadPage, 5000);
 });
--- app/views/messages/index.json.jbuilder
+++ app/views/messages/index.json.jbuilder
@@ -1,7 +1,7 @@
-json.array! @new_messages do |new_message|
-  json.id new_message.id
-  json.content new_message.content
-  json.image new_message.image.url
-  json.created_at = new_message.created_at.strftime('%Y/%m/%d %H:%M:%S')
-  json.user_name = new_message.user.name
+json.array! @new_messages  do |new_message|
+  json.id         new_message.id
+  json.user_name  new_message.user.name
+  json.content    new_message.content
+  json.image      new_message.image.url
+  json.created_at new_message.created_at.strftime('%Y/%m/%d %H:%M')
 end
